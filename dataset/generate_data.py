98ef32d8418703743a46b1d02430c3273f6410e2
ERUD1T3 <josiasmoukpe@gmail.com>
2019-02-09 11:29:24 -0500

    finished data generation

---
+++ dataset/generate_data.py
@@ -0,0 +1,39 @@
+#!/home/th3rudite/anaconda3/bin/python
+import random
+#import torch
+
+
+def generateTenDigitList():
+    '''
+        generate a list of ten number
+    '''
+    random.seed()
+    List = []
+    #stochastically generate a 10 numbers between 0 and 9, 
+    #adds them to List
+    for i in range(10): List.append(random.randint(0, 9)) 
+
+    # print(List)
+    return List
+
+def generateDataset(size):
+    '''
+        Generate data to output file
+    '''
+    
+    with open('data.txt', 'a') as file:
+        for i in range(size + 1):
+            Unsorted = generateTenDigitList() #unsorted generated list
+            Sorted = Unsorted.copy() #copy the unsorted
+            Sorted.sort() #sort the copy
+            file.write(str(Unsorted)) #write the list in file
+            file.write(" ; ") #delimited between sorted and unsorted
+            file.write(str(Sorted)) # write sorted to the file
+            file.write("\n") 
+            #print(Unsorted)
+            #print(Sorted)
+            #file.closed
+
+
+generateDataset(1000000)
+
