683e7fb7fc477a48a0c4f7d0c9df5d6312dfb735
ERUD1T3 <josiasmoukpe@gmail.com>
2019-02-12 19:32:38 -0500

    added the mess

--- dataset/generate_data.py
+++ dataset/generate_data.py
@@ -20,20 +20,22 @@ def generateDataset(size):
     '''
         Generate data to output file
     '''
-    
-    with open('data.txt', 'a') as file:
-        for i in range(size + 1):
-            Unsorted = generateTenDigitList() #unsorted generated list
-            Sorted = Unsorted.copy() #copy the unsorted
-            Sorted.sort() #sort the copy
-            file.write(str(Unsorted)) #write the list in file
-            file.write(" ; ") #delimited between sorted and unsorted
-            file.write(str(Sorted)) # write sorted to the file
-            file.write("\n") 
-            #print(Unsorted)
-            #print(Sorted)
-            #file.closed
+    infile = open('inputdata.txt', 'a')
+    outfile = open('outputdata.txt', 'a')
+
+    for i in range(size + 1):
+        Unsorted = generateTenDigitList() #unsorted generated list
+        Sorted = Unsorted.copy() #copy the unsorted
+        Sorted.sort() #sort the copy
+        infile.write(str(Unsorted)) #write the list in file
+        infile.write("\n") 
+        # file.write(" ; ") #delimited between sorted and unsorted
+        outfile.write(str(Sorted)) # write sorted to the file
+        outfile.write("\n") 
+        #print(Unsorted)
+        #print(Sorted)
+        #file.closed
 
 
-generateDataset(1000000)
+generateDataset(1000)
 
